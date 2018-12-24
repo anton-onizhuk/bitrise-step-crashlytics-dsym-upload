746e2deab52df1f7f70e8378964104186a2cc443
Anton Onizhuk <a.onizhuk@crysberry.com>
2018-12-24 21:29:00 +0200

    no message

--- step.sh
+++ step.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 set -ex
 
-if [ -s executable_path ]; then
+echo "executable_path: ${executable_path}"
+echo "google_service_plist: ${google_service_plist}"
+echo "dsym_dirs: ${dsym_dirs}"
+
+if [ -s "${executable_path}" ]; then
     echo "[!] Can't find Fabric executable"
     echo "[!] File doesn't exists at path '${executable_path}'"
     exit 1
 fi
 
-if [ -s google_service_plist ]; then
+if [ -s "${google_service_plist}" ]; then
     echo "[!] Can't find GoogleService-Info.plist"
     echo "[!] File doesn't exists at path '${google_service_plist}'"
     exit 1
 fi
 
-if [ -d dsym_dirs ]; then
+if [ -d "${dsym_dirs}" ]; then
     echo "[!] Can't find dSYM files"
     echo "[!] Dir doesn't exists at path '${dsym_dirs}'"
     exit 1
 fi
 
-"${executable_path}" -gsp "${google_service_plist}" -p ios "#{dsym_dirs}"
+"${executable_path}" -gsp "${google_service_plist}" -p ios "${dsym_dirs}"
 
 exit 0
